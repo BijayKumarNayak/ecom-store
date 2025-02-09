992d840d915d3ea911ce7ddcaaf4362b1d3921dd
Bijay kuma Nayak <bijayanayak558@gmail.com>
2025-02-09 13:57:55 +0530

    added some dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       gradientColorStops: {
-        'custom-gradient': 'linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)',
+        "custom-gradient":
+          "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)",
       },
-      boxShadow:{
-        'custom-shadow':'box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px'
-      }
+      boxShadow: {
+        "custom-shadow": "box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [addVariablesForColors],
+};
 
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
